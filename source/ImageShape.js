28f300ba1ade042314b28d772ab159cf732caf07
DRN999 <nnakano@ucsc.edu>
2016-12-04 12:50:26 -0800

    imageshape

--- source/ImageShape.js
+++ source/ImageShape.js
@@ -47,7 +47,6 @@ function handleTextureLoaded(image, texture)
 {
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-	console.log(image);
 	if(isPowerOf2(image.width) && isPowerOf2(image.height))
 	{
 		gl.generateMipmap(gl.TEXTURE_2D);
